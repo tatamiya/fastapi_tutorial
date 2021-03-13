126e819029282250c6d281707add6e90a6e35caa
tatamiya <tamyuji1002@gmail.com>
2021-03-13 12:46:58 +0900

    Query Parameters and String Validations

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from pydantic import BaseModel
 
 
@@ -14,7 +14,19 @@ app = FastAPI()
 
 
 @app.post("/items/{item_id}")
-async def create_item(item_id: int, item: Item, q: Optional[str] = None):
+async def create_item(
+    item_id: int,
+    item: Item,
+    q: Optional[str] = Query(
+        ...,  # interpreted as required
+        title="Query string",
+        alias="item-query",
+        description="Query string for the items to search in the database that have a good match",
+        min_length=3,
+        regex="^fixedquery$",
+        deprecated=True,
+    ),
+):
     item_dict = item.dict()
     if item.tax:
         price_with_tax = item.price + item.tax
