69edb76536bf8604082ac9a930c8266f437a90a1
tatamiya <tamyuji1002@gmail.com>
2021-03-13 13:00:33 +0900

    Path Parameters and Numeric Validations

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Path
 from pydantic import BaseModel
 
 
@@ -11,6 +11,18 @@ class Item(BaseModel):
 
 
 app = FastAPI()
+
+
+@app.get("/items/{item_id}")
+async def read_items(
+    item_id: int = Path(..., title="The ID of the item to get", gt=0, le=1000),
+    q: Optional[str] = Query(None, alias="item-query"),
+    size: float = Query(..., gt=0, lt=10.5),
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
 
 
 @app.post("/items/{item_id}")
