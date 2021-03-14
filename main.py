4aeb6af8b74609d14aac363fd1f076c003352101
tatamiya <tamyuji1002@gmail.com>
2021-03-14 14:58:21 +0900

    Extra Models

--- main.py
+++ main.py
@@ -31,22 +31,26 @@ class Offer(BaseModel):
     items: List[Item]
 
 
-class User(BaseModel):
+class UserBase(BaseModel):
     username: str
+    email: EmailStr
     full_name: Optional[str] = None
 
 
-class UserIn(BaseModel):
-    username: str
+class User(UserBase):
+    pass
+
+
+class UserIn(UserBase):
     password: str
-    email: EmailStr
-    full_name: Optional[str] = None
 
 
-class UserOut(BaseModel):
-    username: str
-    email: EmailStr
-    full_name: Optional[str] = None
+class UserOut(UserBase):
+    pass
+
+
+class UserInDB(UserBase):
+    hashed_password: str
 
 
 app = FastAPI()
@@ -151,6 +155,18 @@ async def create_offer(offer: Offer):
     return offer
 
 
+def fake_password_hasher(raw_password: str):
+    return "supersecret" + raw_password
+
+
+def fake_save_user(user_in: UserIn):
+    hashed_password = fake_password_hasher(user_in.password)
+    user_in_db = UserInDB(**user_in.dict(), hashed_password=hashed_password)
+    print("User saved! ..not really")
+    return user_in_db
+
+
 @app.post("/user/", response_model=UserOut)
-async def create_user(user: UserIn):
-    return user
+async def create_user(user_in: UserIn):
+    user_saved = fake_save_user(user_in)
+    return user_saved
