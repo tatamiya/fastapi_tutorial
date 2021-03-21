1a34d62dec7c2787a53ed5ae2c324a713a9808e5
tatamiya <tamyuji1002@gmail.com>
2021-03-21 13:38:29 +0900

    Testing a Database

--- advanced/README.md
+++ advanced/README.md
@@ -6,3 +6,5 @@
   - [Templates](https://fastapi.tiangolo.com/advanced/templates/)
 - testing_dependencies_with_overrides.py
   - [Testing Dependencies with Overrides](https://fastapi.tiangolo.com/advanced/testing-dependencies/)
+- ../sql_app/tests/
+  - [Testing a Database](https://fastapi.tiangolo.com/advanced/testing-database/)
--- sql_app/main.py
+++ sql_app/main.py
@@ -34,6 +34,14 @@ def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return users
 
 
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+
 @app.post("/users/{user_id}/items", response_model=schemas.Item)
 def create_item_for_user(
     user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
