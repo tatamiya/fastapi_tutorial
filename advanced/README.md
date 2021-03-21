b8d5750992b701819215677918fce0987439eae3
tatamiya <tamyuji1002@gmail.com>
2021-03-21 12:47:18 +0900

    Testing Dependencies with Overrides

--- advanced/README.md
+++ advanced/README.md
@@ -4,3 +4,5 @@
 
 - templates/
   - [Templates](https://fastapi.tiangolo.com/advanced/templates/)
+- testing_dependencies_with_overrides.py
+  - [Testing Dependencies with Overrides](https://fastapi.tiangolo.com/advanced/testing-dependencies/)
---
+++ advanced/testing_dependencies_with_overrides/tests/test_main.py
@@ -0,0 +1,41 @@
+from typing import Optional
+
+from fastapi.testclient import TestClient
+
+from ..main import app, common_parameters
+
+client = TestClient(app)
+
+
+async def override_dependency(q: Optional[str] = None):
+    return {"q": q, "skip": 5, "limit": 10}
+
+
+app.dependency_overrides[common_parameters] = override_dependency
+
+
+def test_override_in_items():
+    response = client.get("/items/")
+    assert response.status_code == 200
+    assert response.json() == {
+        "message": "Hello Items!",
+        "params": {"q": None, "skip": 5, "limit": 10},
+    }
+
+
+def test_override_in_items_with_q():
+    response = client.get("/items/?q=foo")
+    assert response.status_code == 200
+    assert response.json() == {
+        "message": "Hello Items!",
+        "params": {"q": "foo", "skip": 5, "limit": 10},
+    }
+
+
+def test_override_in_items_with_params():
+    response = client.get("/items/?q=foo&skip=100&limit=200")
+    assert response.status_code == 200
+    assert response.json() == {
+        "message": "Hello Items!",
+        "params": {"q": "foo", "skip": 5, "limit": 10},
+    }
