4f6466e7b86e77f26c1ada265d65cf9794ffcd3f
tatamiya <tamyuji1002@gmail.com>
2021-10-06 20:58:05 +0900

    GraphQL: define a query with arguments

--- advanced/graphql/main.py
+++ advanced/graphql/main.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import List
+from typing import List, Optional
 
 import strawberry
 from fastapi import FastAPI
@@ -25,20 +25,39 @@ class Skill:
     experience: int
 
 
+users = [
+    User(
+        first_name="Patrick",
+        last_name="Foo",
+        age=100,
+        skills=[
+            Skill(language="Python", experience=5),
+            Skill(language="Go", experience=2),
+            Skill(language="Rust", experience=1),
+        ],
+    ),
+    User(
+        first_name="Michael",
+        last_name="Hoge",
+        age=25,
+        skills=[
+            Skill(language="Python", experience=20),
+            Skill(language="HTML", experience=10),
+            Skill(language="JavaScript", experience=10),
+            Skill(language="GraphQL", experience=5),
+        ],
+    ),
+]
+
+
 @strawberry.type
 class Query:
     @strawberry.field
-    def user(self) -> User:
-        return User(
-            first_name="Patrick",
-            last_name="Foo",
-            age=100,
-            skills=[
-                Skill(language="Python", experience=5),
-                Skill(language="Go", experience=2),
-                Skill(language="Rust", experience=1),
-            ],
-        )
+    def user(self, first_name: str, last_name: str) -> Optional[User]:
+        for user in users:
+            if user.first_name == first_name and user.last_name == last_name:
+                return user
+        return None
 
 
 schema = strawberry.Schema(query=Query)
